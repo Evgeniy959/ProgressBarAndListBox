b0c17e992d671c99f1ffa1867b405e3065f280de
Pili_zr36 <Pili_zr36@itstepru.onmicrosoft.com>
2022-01-28 10:12:44 +0700

    Add project files.

---
+++ ProgressBar/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProgressBar
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string path = null;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        /*private void OpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            var pathFile = OpenFile();
+            TextFile.Text = File.ReadAllText(pathFile);
+            path = pathFile;
+        }
+        private string OpenFile()
+        {
+            var openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Text Files (*.txt)|*.txt";
+            return openFileDialog.ShowDialog() == true ? openFileDialog.FileName : string.Empty;
+        }
+        private int Count()
+        {
+            ProgressBar progbar = new ProgressBar();
+            progbar.IsIndeterminate = false;
+            
+        }*/
+    }
+}
