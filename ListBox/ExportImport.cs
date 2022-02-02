1907298ec19a00027274ddb6e6ac8cb4551c2f73
Pili_zr36 <Pili_zr36@itstepru.onmicrosoft.com>
2022-02-02 13:18:32 +0700

    update

---
+++ ListBox/ExportImport.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace ListBox
+{
+    public class ExportImport
+    {
+        public static void ExportToTXTFile(string str)
+        {
+            var list = new MainWindow();
+            var path = OpenFile();
+            using var file = new StreamWriter(path, append: true);
+            foreach (var item in list._list)
+                file.WriteLine(item);
+        }
+        //public static string ImportToTXTFile(string path )
+        /*public static string ImportToTXTFile(string str)
+        {
+            var path = OpenFile();
+            //path = OpenFile();
+            using var file = new StreamReader(path);
+            //string str = File.ReadAllText(path);
+            //string[] str = File.ReadAllLines(path);
+            //string str = null;
+            while (file.EndOfStream != true) 
+            {
+                str = file.ReadLine();
+                //return str;
+            }
+                //str = file.ReadLine();
+            //str = File.ReadAllText(path);
+            return str;
+        }*/
+        //public static void ExportToXMLFile(ObservableCollection<Student> students, string path)
+        /*public static void ExportToXMLFile(string str, string path)
+        {
+            using var file = new StreamWriter(path, append: false);
+            var xml = new XmlSerializer(str.GetType());
+            xml.Serialize(file, str);
+
+        }
+        //public static ObservableCollection<Student> ImportFromXMLFile(string path)
+        public static string ImportFromXMLFile(string path)
+        {
+            using var file = new StreamReader(path);
+            var xml = new XmlSerializer(typeof(string));
+            return (string)xml.Deserialize(file);
+        }*/
+        public static string OpenFile()
+        {
+            var openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Text Files (*.txt)|*.txt";
+            return openFileDialog.ShowDialog() == true ? openFileDialog.FileName : string.Empty;
+        }
+    }
+}
--- ListBox/MainWindow.xaml.cs
+++ ListBox/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,20 +23,43 @@ namespace ListBox
     /// </summary>
     public partial class MainWindow : Window
     {
-        private ObservableCollection<string> _list;
+        public ObservableCollection<string> _list;
         public MainWindow()
         {
             InitializeComponent();
             _list = new ObservableCollection<string>();
             List.ItemsSource = _list;
         }
+        private void OpenButton_Click(object sender, RoutedEventArgs e)
+        {
+            var path = OpenFile();
+            //StreamReader file = new StreamReader(path);
+            using var file = new StreamReader(path);
+            /*string line;
+            while ((line = file.ReadLine()) != null)*/
+            while (file.EndOfStream != true)
+            {
+                _list.Add(file.ReadLine());
+            }
+            //ExportImport.ImportToTXTFile(List.Items);
+        }
         private void AddButton_Click(object sender, RoutedEventArgs e)
         {
+            //string str = null;
             MessageBoxResult messageInfo;
             string messageBox = "Пользователь уже существует";
             if (!_list.Contains(AddText.Text))
             {
                 _list.Add(AddText.Text);
+                //ExportImport.ExportToTXTFile(AddText.Text);
+                //ExportImport.ExportToTXTFile(_list.Contains));
+                //ExportImport.ExportToTXTFile(_list.ToString());
+
+                //var path = "NEW.txt";
+                var path = OpenFile();
+                using var file = new StreamWriter(path, append: false);
+                foreach (var item in _list)
+                    file.WriteLine(item);
             }
             else messageInfo = MessageBox.Show(messageBox);
         }
@@ -45,6 +70,30 @@ namespace ListBox
                 AddText.Text = List.SelectedItem.ToString();
                 _list.Remove(List.SelectedItem.ToString());                
             }
+        }
+        //public static string ImportToTXTFile(string path )
+        /*public static string ImportToTXTFile()
+        {
+            var path = OpenFile();
+            //path = OpenFile();
+            using var file = new StreamReader(path);
+            //string str = File.ReadAllText(path);
+            //string[] str = File.ReadAllLines(path);
+            //string str = null;
+            while (file.EndOfStream != true)
+            {
+                str = file.ReadLine();
+                //return str;
+            }
+            //str = file.ReadLine();
+            //str = File.ReadAllText(path);
+            return str;
+        }*/
+        private string OpenFile()
+        {
+            var openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Text Files (*.txt)|*.txt";
+            return openFileDialog.ShowDialog() == true ? openFileDialog.FileName : string.Empty;
         }
     }
 }
